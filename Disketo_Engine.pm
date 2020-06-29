7ae830e2bbf02b33d7cfbaf70adfd6d96076487d
martlin2cz2 <martlin@seznam.cz>
2020-06-29 17:58:50 +0200

    Preparing the v2 of core/engine
     - listed all the required features
     - created the initial Engine file

---
+++ Disketo_Engine.pm
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+
+package Disketo_Engine; 
+my $VERSION=2.0.0;
+
+use Disketo_Utils; 
+########################################################################
+# The core, engine, base functions for the particular operations over
+# the resources. Specifies the abstract functions, with all the
+# technicalities (logging, progress, ...).
+########################################################################
+
+
+# TODO: load
+# TODO: calculate
+# TODO: group
+# TODO: print
+# TODO: filter
---
+++ Disketo_Instructions.pm
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+
+package Disketo_Instructions; 
+my $VERSION=2.0.0;
+
+use Disketo_Utils; 
+use Disketo_Engine; 
+########################################################################
+# The implementing module of the Engine. Implements the particular 
+# functions, which may be then mapped to instructions.
+########################################################################
+
+# TODO: ...
